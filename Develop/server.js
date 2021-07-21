15a74c34efd6f89c1c03578fc7d07b5793419303
hkidd <harrisonakidd@gmail.com>
2021-07-21 16:49:06 -0500

    Delete note route seems to be working

--- Develop/server.js
+++ Develop/server.js
@@ -2,19 +2,19 @@
 // THEN I am presented with a landing page with a link to a notes page
 
 // WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column (done for us already)
 
 // WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
+// THEN a Save icon appears in the navigation at the top of the page (done for us already)
 
 // WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes (done for us already)
 
 // WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
+// THEN that note appears in the right-hand column (done for us already)
 
 // WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column (done for us already)
 
 // Required dependencies
 const express = require("express");
@@ -22,7 +22,7 @@ const db = require("./db/db.json");
 const path = require("path");
 const fs = require("fs");
 const PORT = 3001;
-const uuid = require('../helpers/uuid');
+const uuid = require("../helpers/uuid");
 
 const app = express();
 
@@ -34,54 +34,76 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // We have GET, POST, and DELETE methods in the index.js for the '/api/notes' route
-app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request received for the api/notes route`);
-    // this route should read the db.json file and return all saved notes as json
-    res.json(db);
+app.get("/api/notes", (req, res) => {
+  console.info(`${req.method} request received for the api/notes route`);
+  // this route should read the db.json file and return all saved notes as json
+  res.json(db);
 });
 
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received for the api/notes route`);
-    // this route should recieve a new note to save on the request body, add it to the db.json file, and then return the new note to the client with a unique id (using uuid, like our previous example)
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received for the api/notes route`);
+  // this route should recieve a new note to save on the request body, add it to the db.json file, and then return the new note to the client with a unique id (using uuid, like our previous example)
 
-    // grabbing the request body and saving that as a "newNote"
-    let newNote = req.body;
+  // grabbing the request body and saving that as a "newNote"
+  let newNote = req.body;
 
-    // need to ad a unique id to this note (using uuid)
-    newNote.id = uuid();
+  // need to ad a unique id to this note (using uuid)
+  newNote.id = uuid();
 
-    // after adding unique id, this note should be added to the db.json file
-    db.push(newNote);
+  // after adding unique id, this note should be added to the db.json file
+  db.push(newNote);
 
-    // Tried append file, but that was making it unhappy.  So overwriting the db file each time a new note is added
-    fs.writeFile(`./db/db.json`, JSON.stringify(db), (err) =>
-      err ? console.error(err) : console.log(`New Note has been written to JSON db`)
-    );
-    // Need to pass along the new db as a response
-    res.send(db);
+  // Tried append file, but that was making it unhappy.  So overwriting the db file each time a new note is added
+  fs.writeFile(`./db/db.json`, JSON.stringify(db), (err) =>
+    err
+      ? console.error(err)
+      : console.log(`New Note has been written to JSON db`)
+  );
+  // Pass along the new db as a response
+  res.send(db);
 });
 
 // Delete route needs to specific note id to delete the correct one
-app.delete('/api/notes/:id', (req, res) => {
-    console.info(`${req.method} request received for the api/notes route`);
+app.delete("/api/notes/:id", (req, res) => {
+  console.info(`${req.method} request received for the api/notes route`);
 
+  // using the id parameter, create a noteId variable
+  let noteId = req.params.id;
+
+  // loop through the entire note database, and find the one note with a matching id
+  for (let i = 0; i < db.length; i++) {
+    const currentNote = db[i];
+
+    if (currentNote.id === noteId) {
+      // splice out one element starting at that noteId (only that note)
+      db.splice(currentNote, 1);
+      res.json(`Note has been deleted`);
+      return;
+    }
+  }
+  // after splicing out the note, write a new file with the updated db array
+  fs.writeFile("./db/db.json", JSON.stringify(db), (err) =>
+    err ? console.log(err) : console.log("Success!")
+  );
+  // Pass along the new db as a response
+  res.send(db);
 });
 
 // Need the get /notes route to return the notes.html file
-app.get('/notes', (req, res) => {
-    console.info(`${req.method} request received for the /notes route`);
-    // the response should be to send the notes.html to the client
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
+app.get("/notes", (req, res) => {
+  console.info(`${req.method} request received for the /notes route`);
+  // the response should be to send the notes.html to the client
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 // Need the get * route to return the index.html file.  THIS SHOULD BE LAST
-app.get('*', (req, res) => {
-    console.info(`${req.method} request received for the /index route`);
-    // the response should be to send the index.html to the client
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+app.get("*", (req, res) => {
+  console.info(`${req.method} request received for the /index route`);
+  // the response should be to send the index.html to the client
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // Need to have the app lsitening at the specified port
-app.listen(PORT, () => 
-console.log(`App listening at http://localhost:${PORT} 😎`)
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 😎`)
 );
