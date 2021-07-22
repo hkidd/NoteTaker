af84d39a8824004de22775aec869eb809c8102fd
hkidd <harrisonakidd@gmail.com>
2021-07-22 15:30:33 -0500

    Moved files up one level for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const db = require("./db/db.json");
 const path = require("path");
 const fs = require("fs");
 const PORT = process.env.PORT || 3001;
-const uuid = require("../helpers/uuid");
+const uuid = require("./helpers/uuid");
 
 // Initialize express application
 const app = express();
